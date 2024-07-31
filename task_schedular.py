c0b25727d1c8414e67aa30e7fd1df9f32f1e2285
bo.song <bo.song@gotogameday.com>
2024-07-31 11:23:23 +0800

    update

--- task_schedular.py
+++ task_schedular.py
@@ -4,7 +4,7 @@ from task_profit import ProfitSum
 scheduler = BlockingScheduler()
 
 
-scheduler.add_job(ProfitSum().ana_profit, 'cron', hour=9, minute=0, second=0)
+scheduler.add_job(ProfitSum().ana_profit, 'cron', hour=11, minute=26, second=0)
 
 
 scheduler.start()
